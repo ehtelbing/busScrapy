5b197c44a3f49f6accd64f5a1b0f58dc4598e8e7
hanmingcheng <12345678>
2019-07-16 08:23:19 +0800

    bussniss_Scrapy

---
+++ busScrapy/urls.py
@@ -0,0 +1,25 @@
+"""busScrapy URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+from fScrapy import views
+
+urlpatterns = [
+    path(r'', views.index),
+    # 调取views.py页面函数返回的页面路径
+    url(r'^your$', views.hello)  # http://127.0.0.1:8001/your   '^'匹配路径开始+匹配路径名字字符等+'$'匹配路径结束
+]
